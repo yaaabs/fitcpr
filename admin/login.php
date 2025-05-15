67544fa5042c7b0ef699f4d377217e37df3b3159
yaaabs <brianyabut02@gmail.com>
2025-05-15 20:54:50 +0800

    Login pages are responsive now

--- admin/login.php
+++ admin/login.php
@@ -1,110 +1,152 @@
 <?php require_once('../config.php') ?>
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
- <?php require_once('inc/header.php') ?>
-<body class="hold-transition ">
+ <?php require_once('../inc/header.php') ?>
+<body class="hold-transition login-page">
   <script>
     start_loader()
   </script>
   <style>
-    html, body{
-      height:calc(100%) !important;
-      width:calc(100%) !important;
+    html, body {
+        height: calc(100%) !important;
+        width: calc(100%) !important;
     }
-    body{
-      background-image: url("<?php echo validate_image($_settings->info('cover')) ?>");
-      background-size:cover;
-      background-repeat:no-repeat;
+    
+    body {
+        background-image: url("<?php echo validate_image($_settings->info('cover')) ?>");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
     }
-    .login-title{
-      text-shadow: 2px 2px black
+    
+    .login-page {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 15px;
+        height: 100%;
     }
-    #login{
-      flex-direction:column !important
+    
+    .btn-primary {
+        background-color: #A7CA3B;
+        border-color: #A7CA3B;
+        color: white;
+        padding: 10px;
+        font-size: 18px;
     }
-    #logo-img{
-        height:150px;
-        width:150px;
-        object-fit:scale-down;
-        object-position:center center;
-        border-radius:100%;
+    
+    .btn-primary:hover {
+        background-color: #95b833;
+        border-color: #95b833;
+        color: white;
     }
-    #login .col-7,#login .col-5{
-      width: 100% !important;
-      max-width:unset !important
+    
+    /* Card styling */
+    .login-card {
+        border-radius: 10px;
+        overflow: hidden;
+        box-shadow: 0 5px 15px rgba(0,0,0,0.2);
+        width: 100%;
+        max-width: 400px;
+        margin: 0 auto;
+        background-color: white;
+    }
+    
+    .card-header {
+        background-color: #009900;
+        color: white !important;
+        padding: 20px;
+        border-bottom: none;
+    }
+    
+    .card-header h4 {
+        color: white !important;
+        margin: 0;
+        font-size: 24px;
+    }
+    
+    .card-body {
+        padding: 25px;
+    }
+    
+    /* Form elements */
+    .form-control {
+        border-radius: 5px;
+        padding: 12px;
+        height: auto;
+    }
+    
+    .input-group-text {
+        background-color: #f8f9fa;
+        border-radius: 0 5px 5px 0;
+        padding-left: 15px;
+        padding-right: 15px;
+    }
+    
+    /* Links */
+    a.text-success {
+        color: #009900 !important;
+        font-weight: 600;
+    }
+    
+    /* Responsive styles */
+    @media (max-width: 576px) {
+        .login-card {
+            max-width: 100%;
+        }
+        
+        .card-body {
+            padding: 20px 15px;
+        }
+        
+        .form-control, .btn {
+            font-size: 16px;
+        }
     }
   </style>
-  <div class="h-100 d-flex align-items-center w-100" id="login">
-    <div class="col-7 h-100 d-flex align-items-center justify-content-center">
-      <div class="w-100">
-        <center><img src="<?= validate_image($_settings->info('logo')) ?>" alt="" id="logo-img"></center>
-        <h1 style="
-    font-family: 'Montserrat', sans-serif; 
-    font-weight: 700; 
-    font-size: 4.5rem; 
-    color: #FDB813; 
-    text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5); 
-    text-align: center; 
-    width: 100%; 
-    margin: 0 auto; 
-    line-height: 1.2;">
-    <br>
-    <b>Welcome Admin!</b>
-</h1>
-      </div>
-      
+
+  <div class="login-card">
+    <div class="card-header text-center">
+        <h4><b>Admin Login</b></h4>
     </div>
-    
-    <div class="col-5 h-100 bg-gradient">
-      <div class="d-flex w-100 h-100 justify-content-center align-items-center">
-        <div class="card col-sm-12 col-md-6 col-lg-3 card-outline card-warning">
-          <div class="card-header">
-            <h4 class="text-dark text-center"><b>Login</b></h4>
-          </div>
-          <div class="card-body">
-            <form id="login-frm" action="" method="post">
-              <div class="input-group mb-3">
-                <input type="text" class="form-control" autofocus name="username" placeholder="Username">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-user"></span>
-                  </div>
-                </div>
-              </div>
-              <div class="input-group mb-3">
-                <input type="password" class="form-control" name="password" placeholder="Password">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-lock"></span>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-8">
-                <a href="<?= base_url ?>" class="text-decoration-none text-success fw-bold">
-          <br>  <i class="fa fa-home"></i> Go to Website
-        </a>
-                </div>
-                <!-- /.col -->
-                <div class="col-4">
-               <br>   <button type="submit" class="btn btn-success btn-block" style="background-color: #A7CA3B; color: white;">Sign In</button>
-                
-                </div>
-                <!-- /.col -->
-              </div>
-            </form>
+    <div class="card-body">
+      <form id="login-frm" action="" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" name="username" placeholder="Username" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
           </div>
         </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="password" placeholder="Password" required>
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+        </div>
+      </form>
+      <div class="text-center mt-4">
+        <a href="<?= base_url ?>" class="text-success">
+          <i class="fa fa-home"></i> Back to Site
+        </a>
       </div>
     </div>
   </div>
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="<?= base_url ?>plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+<script src="<?= base_url ?>dist/js/adminlte.min.js"></script>
 
 <script>
   $(document).ready(function(){
